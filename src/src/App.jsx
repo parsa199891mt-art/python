926ac1c48f4e8d0827f877e47232ed72bf3cdbe5
parsa199891mt-art <parsa199891mt@gmail.com>
2025-11-30 19:51:31 +0330

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PythonStudio from './PythonStudio';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
+      <PythonStudio />
+    </div>
+  );
+}
